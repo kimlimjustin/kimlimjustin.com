7a54602191f2ac4acc1ffa5a8eaef150a300b0e8
kimlimjustin <kimlimjustin@gmail.com>
2021-03-11 20:04:09 +0700

    Added topnav feature

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-    if(localStorage.getItem('theme')) document.body.dataset.theme = localStorage.getItem('theme')
+    if(document.documentElement.clientWidth <= 480){
+        document.querySelector(".topnav-name").innerText = "<Justin M.K. />"
+    }else{
+        document.querySelector(".topnav-name").innerText = "<Justin Maximillian Kimlim />"
+    }
+    window.addEventListener("resize", () => {
+        if(document.documentElement.clientWidth <= 480){
+            document.querySelector(".topnav-name").innerText = "<Justin M.K. />"
+        }else{
+            document.querySelector(".topnav-name").innerText = "<Justin Maximillian Kimlim />"
+        }
+    })
+    document.querySelector(".topnav-ham").addEventListener("click", () => {
+        document.querySelector(".topnav-menu").style.width = '100vw';
+        document.querySelector(".topnav-exit-menu").addEventListener("click", () => {
+            document.querySelector(".topnav-menu").style.width = '0vw';
+        })
+    })
 })
