939fa3bd1fb3e463bfaccc725278ccafa942d3ad
kimlimjustin <kimlimjustin@gmail.com>
2021-03-13 20:36:16 +0700

    Added copyright and media links

--- script.js
+++ script.js
@@ -23,4 +23,11 @@ document.addEventListener("DOMContentLoaded", () => {
         })
     })
     AOS.init()
+    fetch('https://api.github.com/repos/kimlimjustin/kimlimjustin.github.io')
+    .then(response => response.json())
+    .then(result => {
+        document.querySelector("#github-stars").innerText = result["stargazers_count"]
+        document.querySelector("#github-forks").innerText = result["forks"]
+    })
+    .catch(() => document.querySelector(".github-stats").removeChild(document.querySelector(".github-stats")))
 })
